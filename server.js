1d073591695298bf1f38dffcb08d561334891e23
Vin <vin@black.com>
2024-11-20 16:16:06 -0300

    Alura
    Arquivo das aulas de back-end, criado para a dinamica do node.js.
    VSE.

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from "express";
+import express from `express`;
 
 const posts = [
-    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+    { id: 1, descricao: `Uma foto teste`, imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: `Gato fazendo yoga`, imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: `Gato fazendo panqueca`, imagem: `https://placecats.com/millie/300/150`},
 ];
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log(`Servidor escutando...`);
 });
 
-app.get("/posts", (req, res) => {
+app.get(`/posts`, (req, res) => {
     res.status(200).json(posts);
 });
 
@@ -23,7 +23,7 @@ function buscarPostPorID(id) {
     })
 }
 
-app.get("/posts/:id", (req, res) => {
+app.get(`/posts/:id`, (req, res) => {
     const index = buscarPostPorID(req.params.id)
     res.status(200).json(posts[index]);
 });
